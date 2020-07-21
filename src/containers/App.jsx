fc21bdd3b361622962c0d016aefb44d0fc0acf43
Alberto López <engineer.lopezx2@gmail.com>
2020-07-21 13:31:08 -0500

    Custom Hooks
    React nos permite crear nuestros propios Hooks. Solo debemos seguir algunas convenciones:
    
    Los hooks siempre deben empezar con la palabra use: useAPI, useMovies, useWhatever.
    Si nuestro custom hook nos permite consumir/interactuar con dos elementos (por ejemplo, title y setTitle), nuestro hook debe devolver un array.
    
    Si nuestro custom hook nos permite consumir/interactuar con tres o más elementos (por ejemplo, name, setName, lastName, setLastName, etc.), nuestro hook debe devolver un objeto.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -6,26 +6,20 @@ import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 
+import useInitialState from '../hooks/useInitialState';
+
 import '../assets/styles/App.scss';
 
-const App = () => {
-  const [videos, setVideos] = useState({
-    mylist: [],
-    trends: [],
-    originals: [],
-  });
+const API = 'http://localhost:3000/initialState';
 
-  useEffect(() => {
-    fetch('http://localhost:3000/initialState')
-      .then((response) => response.json())
-      .then((data) => setVideos(data));
-  }, []);
+const App = () => {
+  const initialState = useInitialState(API);
 
   return (
     <div className="App">
       <Header />
       <Search />
-      {videos.mylist.length > 0 && (
+      {initialState.mylist.length > 0 && (
         <Categories title="Mi Lista">
           <Carousel>
             <CarouselItem />
@@ -35,7 +29,7 @@ const App = () => {
 
       <Categories title="Tendencias">
         <Carousel>
-          {videos.trends.map((item) => (
+          {initialState.trends.map((item) => (
             <CarouselItem key={item.id} {...item} />
           ))}
         </Carousel>
